95a3d521158f212e33690eb1401235529264c22b
CyberPanel <support@cyberpanel.net>
2025-04-25 20:55:51 +0200

    Fix TailwindCSS and PostCSS config for Vercel build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#005BBB",
-        secondary: "#FFD500"
-      }
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
