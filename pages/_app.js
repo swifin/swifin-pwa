426f26276f8affb0ee780cd815c64beed8c7dece
CyberPanel <support@cyberpanel.net>
2025-04-25 22:45:06 +0200

    Add homepage content

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import '../styles/globals.css';
 
+import Head from 'next/head';
 import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { WagmiProvider, http } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -22,7 +23,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider chains={[polygonMumbai]}>
+        <RainbowKitProvider>
           <Component {...pageProps} />
         </RainbowKitProvider>
       </QueryClientProvider>
@@ -30,4 +31,14 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
+<Head>
+  <title>Swifin Wallet - Empowering Inclusive Prosperity</title>
+  <meta name="description" content="Activate your Swifin Wallet and join the global movement for economic transformation." />
+  <meta property="og:title" content="Swifin Wallet" />
+  <meta property="og:description" content="Activate your Swifin Wallet and join the global movement." />
+  <meta property="og:image" content="/swifin-og-image.png" />
+  <meta name="twitter:card" content="summary_large_image" />
+</Head>
+
 export default MyApp;
+
