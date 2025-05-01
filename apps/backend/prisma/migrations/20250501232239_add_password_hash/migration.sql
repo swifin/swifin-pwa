1697df2361d3327bdc2ce95739ed0f0d9a7921a8
CyberPanel <support@cyberpanel.net>
2025-05-02 01:29:29 +0200

    Add homepage content

---
+++ apps/backend/prisma/migrations/20250501232239_add_password_hash/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "address" TEXT;
+ALTER TABLE "User" ADD COLUMN "birthday" TEXT;
+ALTER TABLE "User" ADD COLUMN "city" TEXT;
+ALTER TABLE "User" ADD COLUMN "mobilePhone" TEXT;
+ALTER TABLE "User" ADD COLUMN "postalCode" TEXT;
